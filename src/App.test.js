79df2795c0dd72d9ed816229de3cb09df7160fd7
Zach Truong <zacharytruong@gmail.com>
2023-08-10 17:46:07 -0500

    update link

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App.js', () => {
-  test('renders learn react link', () => {
+  test('renders learn testing and dev link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByTestId(/learn-link/i);
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test('correct url', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId(/learn-link/i);
+    expect(linkElement.href).toMatch('www.demo.com');
   });
 });
